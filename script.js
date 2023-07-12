05bf1cb9aec368ca338e3069bb4dce3e2616dfe2
brunoabunai <bruno.afonsorr@gmail.com>
2023-07-12 17:53:52 -0300

    update profile picture "alt" with "dark mode"

--- script.js
+++ script.js
@@ -10,10 +10,19 @@ function toggleMode() {
   if (body.classList.contains("light")) {
     // body.classList.remove('light')
     img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "avar image light mode, japa com fundo mais claro, com uma roupa mais cinza"
+    )
+
     // switchImage.setAttribute("src", "./assets/sun.svg")
   } else {
     // body.classList.add('light')
     img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "avatar image dark mode, japa de roupa preta com o fundo laranja"
+    )
     // switchImage.setAttribute("src", "./assets/moon-stars.svg")
   }
 }
